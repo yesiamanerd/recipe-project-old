0641840913c5727bdf73fba6c11b754bea47bcf2
yesiamanerd <yesiamanerd@gmail.com>
2019-07-26 21:50:17 -0400

    added fixing for Lombok

--- src/main/java/guru/springframework/recipeproject/domain/Category.java
+++ src/main/java/guru/springframework/recipeproject/domain/Category.java
@@ -1,11 +1,13 @@
 package guru.springframework.recipeproject.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
 
